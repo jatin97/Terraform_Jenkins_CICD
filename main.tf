4ef3d3cc4012ee893c904535579c7249a98ec7bf
jatin97 <47440231+jatin97@users.noreply.github.com>
2024-07-26 04:28:57 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ terraform {
     bucket         = "jatin-s3bucket-date"
     key            = "globalstate/s3/terraform.tfstate"
     region         = "eu-west-1"
-    dynamodb_table = "jatin-date-dblocks"
+    dynamodb_table = "jatin-25-dblocks"
     encrypt        = true
   }
 }
